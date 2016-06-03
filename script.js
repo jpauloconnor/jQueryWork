e83cc75b21ac8fd64d37ca6531adf76e5c56d387
Paul <jamespauloconnor@gmail.com>
2016-06-02 22:51:13 -0400

    Moving the elephant photo with shake, slide right and left, hide, and fade in

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 $(document).ready(function(){
-	$('div').click(function(){
-		$(this).hide();
+	$('#hideButton').click(function(){
+		$('#elephant').hide();
 	});
-});
-
-$(document).ready(function(){
-	$('vanish').click(function(){
-		$(this).vanish('slow');
+	$('#fadeInButton').click(function(){
+		$('#elephant').fadeIn(2000);
 	});
-});
-
-$(document).ready(function(){
-	$('button').click(function(){
-		$('#fadeIn1').fadeIn(5000);
+	$('#shakeButton').click(function(){
+		$('#elephant').effect("shake");
 	});
+	/*
+	$('bounceButton').click(function(){
+		$('#elephant').bounce();
+	});*/
+	$( "#rightButton" ).click(function() {
+  		$( "#elephant" ).animate({ "left": "+=50px" }, "slow" );
+	});
+ 
+	$( "#leftButton" ).click(function(){
+  		$( "#elephant" ).animate({ "left": "-=50px" }, "slow" );
+	});
+	// $('#rightButton').click(function(){
+	// 	$('#elephant').animate({"left": "+=50px"}, "slow");
+	// });
+	// $('#leftButton').click(function(){
+	// 	$('#elephant').animate({"left": "-=50px"}, "slow");
+	// });
 });
